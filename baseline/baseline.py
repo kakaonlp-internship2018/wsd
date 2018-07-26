a79ca7c79529dc43eb4b52221d1b58b4d30640bc
kakao <kakao@kakaoui-MacBook-Pro.local>
2018-07-26 13:08:57 +0900

    코드 스타일 수정하고 .gitignore 와 requirement 추가하였습니다. #1
    코드 스타일 pylint pep8 스타일에 맞추어 수정하였습니다.
    requirement.txt 에 nltk 추가해두었습니다.
    gitignore에 bin 파일 무시하도록 하였습니다.
    precision 용어를 accuracy로 수정하였습니다.

--- baseline/baseline.py
+++ baseline/baseline.py
@@ -1,16 +1,24 @@
+"""
+baseline evalutator
+__author__ = 'jeff.yu (jeff.yu@kakaocorp.com)'
+__copyright__ = 'No copyright, just copyleft!'
+"""
+
+###########
+# imports #
+###########
 import re
-from nltk.tokenize import word_tokenize
-import nltk
+import sys
 import pickle
 import os
-import sys
+import nltk
 
-''' You need to download nltk package 'punkt' first'''
-#nltk.download('punkt') 
+# You need to download nltk package 'punkt' first
+# nltk.download('punkt')
 
 
-''' FILE PATH '''
-''' train_set.txt and test_set.txt should be in current directory '''
+# FILE PATH #
+# train_set.txt and test_set.txt should be in current directory #
 
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 VOCA = os.path.join(THIS_FOLDER, 'voca.bin')
@@ -20,119 +28,130 @@ ANSWER = os.path.join(THIS_FOLDER, 'answer.txt')
 try:
     TRAIN_SET = os.path.join(THIS_FOLDER, sys.argv[1])
     TEST_SET = os.path.join(THIS_FOLDER, sys.argv[2])
-except:
+except BaseException:
     print('usage: python3 baseline.py transformed_train_file_name transfromed_test_file_name')
     sys.exit(1)
 
 
+#############
+# functions #
+#############
 
-
-''' Function that build homograph vocabulary from training set '''
-''' vocabulary is an dictionary, key = "WORD__NN/POS" value = appearing count '''
-''' the function saves vocabulary as file "voca.bin" in current directory '''
-
-def buildVoca():
-    fr = open(TRAIN_SET, 'r')
+def build_voca():
+    """
+    Function that build homograph vocabulary from training set
+    vocabulary is an dictionary, key = "WORD__NN/POS" value = appearing count
+    the function saves vocabulary as file "voca.bin" in current directory
+    """
+    fr_train = open(TRAIN_SET, 'r')
     fw_voca = open(VOCA, 'wb')
-    tokenList = []
+    token_list = []
     while True:
-        line = fr.readline()
-        if not line: break
-        line = line.replace("\n","")
-        newTokens = re.split('[ ]',line)
-        for token in newTokens:
-            if re.compile('.*__[\d][\d].*').match(token):
-                tokenList.append(token)
-    
-    text = nltk.Text(tokenList)
+        line = fr_train.readline()
+        if not line:
+            break
+        line = line.replace("\n", "")
+        new_tokens = re.split('[ ]', line)
+        for token in new_tokens:
+            if re.compile(r'.*__[\d][\d].*').match(token):
+                token_list.append(token)
+
+    text = nltk.Text(token_list)
     vocabulary = text.vocab()
 
     pickle.dump(vocabulary, fw_voca)
     fw_voca.close()
-    fr.close()
+    fr_train.close()
 
     return
 
 
-''' Function that build MaxFreqDic from vocabulary '''
-''' MaxFreqDic is an dictionary, key = "WORD/POS", value = the most frequent sense of the word (formed "WORD__NN/POS") '''
-''' the function saves MaxFreqDic as file "max_freq_dic.bin" in current directory '''
-
-def buildMaxFreqDic():
+def build_max_freq_dic():
+    """
+    Function that build MaxFreqDic from vocabulary
+    MaxFreqDic is an dictionary,
+    key = "WORD/POS", value = the most frequent sense of the word (formed "WORD__NN/POS")
+    the function saves MaxFreqDic as file "max_freq_dic.bin" in current directory
+    """
     fr_voca = open(VOCA, 'rb')
-    fw_maxFreqDic = open(MAX_FREQ_DIC, 'wb')
+    fw_max_freq_dic = open(MAX_FREQ_DIC, 'wb')
     vocabulary = pickle.load(fr_voca)
-    
 
     keys = vocabulary.keys()
-    keysWithoutSense = set(map(lambda y: re.sub('__[\d][\d]', '', y), keys))
-    maxFreqDic = {}
-    for key in keysWithoutSense:
-        splitKey = re.split('/',key)
-        word = splitKey[0]
-        pos = splitKey[1]
+    keys_without_sense = set(map(lambda y: re.sub(r'__[\d][\d]', '', y), keys))
+    max_freq_dic = {}
+    for key in keys_without_sense:
+        split_key = re.split('/', key)
+        word = split_key[0]
+        pos = split_key[1]
 
-        max = -1
-        maxSense = "UNKNOWN"
-        for n in range(1,99):
-            if n < 10 :
-                targetWord = word+"__0"+str(n)+"/"+pos
-            else :
-                targetWord = word+"__"+str(n)+"/"+pos
-            freq = vocabulary.get(targetWord, -1)
-            if freq > max:
-                max = freq
-                maxSense = targetWord
+        max_freq = -1
+        max_sense = "UNKNOWN"
+        for n in range(1, 99):
+            if n < 10:
+                target_word = word + "__0" + str(n) + "/" + pos
+            else:
+                target_word = word + "__" + str(n) + "/" + pos
+            freq = vocabulary.get(target_word, -1)
+            if freq > max_freq:
+                max_freq = freq
+                max_sense = target_word
 
-        #keyWithSenseList = list(filter(lambda y: re.match(word+"__[\d][\d]/"+pos, y) != None, keys))
-        #if keyWithSenseList != []:
+        #keyWithSenseList =list(filter(lambda y: re.match(word+"__[\d][\d]/"+pos, y) != None, keys))
+        # if keyWithSenseList != []:
         #    freqList = list(map(lambda y: vocabulary[y], keyWithSenseList))
-        #    maxSense = keyWithSenseList[freqList.index(max(freqList))]
-        #    maxFreqDic[key] = maxSense
-        
-        maxFreqDic[key] = maxSense
-    
-    pickle.dump(maxFreqDic, fw_maxFreqDic)
+        #    max_sense = keyWithSenseList[freqList.index(max(freqList))]
+        #    max_freq_dic[key] = max_sense
 
-    fw_maxFreqDic.close()
+        max_freq_dic[key] = max_sense
+
+    pickle.dump(max_freq_dic, fw_max_freq_dic)
+
+    fw_max_freq_dic.close()
     fr_voca.close()
 
-''' Function that makes answer from test set '''
-''' for each homograph word, this function chooses most frequent sense of the word '''
-''' if the homograph word is not trained before, the function chooses "UNKNOWN" as answer word. and it will cause wrong answering in evaluation '''
-''' answer sentences are saved as file, "answer.txt" in current directory '''
 
-def makeAnswer():
+def make_answer():
+    """
+    # Function that makes answer from test set #
+    # for each homograph word, this function chooses most frequent sense of the word #
+    # if the homograph word is not trained before, the function chooses "UNKNOWN" as answer word.
+    and it will cause wrong answering in evaluation #
+    # answer sentences are saved as file, "answer.txt" in current directory #
+    """
     fr_test = open(TEST_SET, 'r')
     fw_answer = open(ANSWER, 'w')
-    fr_maxFreqDic = open(MAX_FREQ_DIC, 'rb')
-    maxFreqDic = pickle.load(fr_maxFreqDic)
+    fr_max_freq_dic = open(MAX_FREQ_DIC, 'rb')
+    max_freq_dic = pickle.load(fr_max_freq_dic)
 
     while True:
         line = fr_test.readline()
-        if not line: break
-        line = line.replace("\n","")
-        tokens = re.split('[ ]',line)
+        if not line:
+            break
+        line = line.replace("\n", "")
+        tokens = re.split('[ ]', line)
 
         for token in tokens:
-            if re.compile('.*__[\d][\d].*').match(token):
-                queryWord = re.sub('__[\d][\d]', '', token)
-                answerWord = maxFreqDic.get(queryWord, "UNKNOWN")
+            if re.compile(r'.*__[\d][\d].*').match(token):
+                query_word = re.sub(r'__[\d][\d]', '', token)
+                answer_word = max_freq_dic.get(query_word, "UNKNOWN")
             else:
-                answerWord = token
-            fw_answer.write(answerWord+" ")
+                answer_word = token
+            fw_answer.write(answer_word + " ")
 
         fw_answer.write('\n')
 
-    fr_maxFreqDic.close()
+    fr_max_freq_dic.close()
     fr_test.close()
     fw_answer.close()
     return
 
 
-''' evaluate answer by comparing "test_set.txt" with "answer.txt" '''
-''' only homograph words are counted for scoring '''
 def evaluate():
+    """
+    # evaluate answer by comparing "test_set.txt" with "answer.txt" #
+    # only homograph words are counted for scoring #
+    """
     fr_test = open(TEST_SET, 'r')
     fr_answer = open(ANSWER, 'r')
 
@@ -140,38 +159,35 @@ def evaluate():
     correct = 0
 
     while True:
-        testLine = fr_test.readline().replace("\n","")
-        answerLine = fr_answer.readline().replace("\n","")
+        test_line = fr_test.readline().replace("\n", "")
+        answer_line = fr_answer.readline().replace("\n", "")
 
-        if not testLine or not answerLine : break
+        if not test_line or not answer_line:
+            break
 
-        testTokens = re.split('[ ]', testLine)
-        answerTokens = re.split('[ ]', answerLine)
+        test_tokens = re.split('[ ]', test_line)
+        answer_tokens = re.split('[ ]', answer_line)
 
-        mergedTokens = zip(testTokens, answerTokens)
+        merged_tokens = zip(test_tokens, answer_tokens)
 
-        for token1, token2 in mergedTokens:
-            if re.compile('.*__[\d][\d].*').match(token1):
-                count = count+1
+        for token1, token2 in merged_tokens:
+            if re.compile(r'.*__[\d][\d].*').match(token1):
+                count = count + 1
                 if token1 == token2:
-                    correct = correct+1
+                    correct = correct + 1
 
-    print("The number of homograph : ",count)
-    print("The number of correct answer : ",correct)
-    print("Precision : ",(correct/count)*100)
-        
-
+    print("The number of homograph : ", count)
+    print("The number of correct answer : ", correct)
+    print("Accuracy : ", (correct / count) * 100)
 
 
-buildVoca()
+build_voca()
 print("building voca done!")
 
-buildMaxFreqDic()
-print("building maxFreqDic done!")
+build_max_freq_dic()
+print("building max_freq_dic done!")
 
-makeAnswer()
+make_answer()
 print("answering done!")
 
 evaluate()
-
-
--- baseline/transform.py
+++ baseline/transform.py
@@ -1,3 +1,8 @@
+"""
+transform.py
+__author__ = 'jeff.yu (jeff.yu@kakaocorp.com)'
+__copyright__ = 'No copyright, just copyleft!'
+"""
 import re
 import os
 import sys
@@ -6,23 +11,24 @@ THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 REMOVE_SENSE = False
 
 try:
-    fr = open(sys.argv[1], 'r')
-    fw = open(sys.argv[2], 'w')
-except:
+    FR = open(sys.argv[1], 'r')
+    FW = open(sys.argv[2], 'w')
+except BaseException:
     print('usage: python3 transform.py source_file_name dist_file_name')
     sys.exit(1)
 
-count = 0
+COUNT = 0
 while True:
-    line = fr.readline()
-    if not line: break
-    if count%3 == 1:
-        line = line.replace("\n","")
-        if REMOVE_SENSE : line = re.sub('__[\d][\d]',"",line)
-        line = re.split('[+ ]',line)
-        fw.write(' '.join(line)+"\n")
-    count = count+1
+    LINE = FR.readLINE()
+    if not LINE:
+        break
+    if COUNT % 3 == 1:
+        LINE = LINE.replace("\n", "")
+        if REMOVE_SENSE:
+            LINE = re.sub(r'__[\d][\d]', "", LINE)
+        LINE = re.split('[+ ]', LINE)
+        FW.write(' '.join(LINE)+"\n")
+    COUNT = COUNT+1
 
-fr.close()
-fw.close()
-
+FR.close()
+FW.close()
